2823b5e729d84b8972ee4cdf895d03e7807b7f5e
SangLT <thanhsang095@gmail.com>
2020-05-17 15:43:21 +0700

    test

--- app/src/main/java/com/nus/SmsCallManager/Utils/Constants.java
+++ app/src/main/java/com/nus/SmsCallManager/Utils/Constants.java
@@ -1,6 +1,5 @@
 package com.nus.SmsCallManager.Utils;
-// test master
-
+// test
 public class Constants {
     public static final String TAG = "SmsCallManager";
     public static final String CALL_LOG_PREF = "CALL_LOG_PREF";
